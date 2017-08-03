e96fdf3b72217eefb9bacf6d8ce2b879b6208e86
Radhika Kondabala <Radhika.Kondabala@kiewit.com>
2017-08-03 21:00:44 +0530

    Add project files.

---
+++ WebApp-Storage-DotNet/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace WebApp_Storage_DotNet
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ WebApp-Storage-DotNet/Controllers/HomeController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.Azure;
+
+namespace WebApp_Storage_DotNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        static CloudBlobClient blobClient;
+        const string blobContainerName = "webappstoragedotnet-imagecontainer";
+        static CloudBlobContainer blobContainer;
+
+        /// <summary> 
+        /// 
+        /// </summary> 
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                // Retrieve storage account information from connection string
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+                // Create a blob client for interacting with the blob service
+                blobClient = storageAccount.CreateCloudBlobClient();
+                blobContainer = blobClient.GetContainerReference(blobContainerName);
+                await blobContainer.CreateIfNotExistsAsync();
+
+                await blobContainer.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+
+                List<Uri> allBlobs = new List<Uri>();
+                foreach (IListBlobItem blob in blobContainer.ListBlobs())
+                {
+                    if (blob is CloudBlockBlob)
+                        allBlobs.Add(blob.Uri);
+                }
+
+                return View(allBlobs);
+            }
+            catch (Exception ex)
+            {
+                ViewData["message"] = ex.Message;
+                ViewData["trace"] = ex.StackTrace;
+                return View("Error");
+            } 
+        }
+
+        /// <summary> 
+        /// 
+        /// </summary> 
+        [HttpPost]
+        public async Task<ActionResult> UploadAsync()
+        {
+            try
+            {
+                HttpFileCollectionBase files = Request.Files;
+
+
+                int fileCount = files.Count;
+
+                if (fileCount > 0)
+                {
+                    for (int i = 0; i < fileCount; i++)
+                    {
+                        var fileName = Path.GetFileName(files[i].FileName);
+                        var path = Path.Combine(Server.MapPath("~/Images"), fileName);
+                        files[i].SaveAs(path);
+                        
+                        CloudBlockBlob blob = blobContainer.GetBlockBlobReference(GetRandomBlobName(fileName));
+                       
+                        await blob.UploadFromFileAsync(path, FileMode.Open);
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewData["message"] = ex.Message;
+                ViewData["trace"] = ex.StackTrace;
+                return View("Error");
+            }            
+        }
+
+        /// <summary> 
+        ///
+        /// </summary> 
+        [HttpPost]
+        public async Task<ActionResult> DeleteImage(string name)
+        {
+            try
+            {
+                Uri uri = new Uri(name);
+                string filename = Path.GetFileName(uri.LocalPath);
+
+                var blob = blobContainer.GetBlockBlobReference(filename);
+                await blob.DeleteIfExistsAsync();
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewData["message"] = ex.Message;
+                ViewData["trace"] = ex.StackTrace;
+                return View("Error");
+            }
+        }
+
+        /// <summary> 
+        ///
+        /// </summary> 
+        [HttpPost]
+        public async Task<ActionResult> DeleteAll()
+        {
+            try
+            {
+                foreach (var blob in blobContainer.ListBlobs())
+                {
+                    if (blob is CloudBlockBlob)
+                    {
+                        await ((CloudBlockBlob)blob).DeleteIfExistsAsync();
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewData["message"] = ex.Message;
+                ViewData["trace"] = ex.StackTrace;
+                return View("Error");
+            }
+        }
+
+        /// <summary> 
+        /// 
+        /// </summary> 
+        private string GetRandomBlobName(string filename)
+        {
+            string ext = Path.GetExtension(filename);
+            return string.Format("{0:10}_{1}{2}", DateTime.Now.Ticks, Guid.NewGuid(), ext);
+        }
+    }
+}
